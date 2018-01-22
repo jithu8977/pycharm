7b64f981fee9165d6fee4f450b9391d176a29c5d
inwkstudent <mandeep62.msharma@dal.ca>
2018-01-22 15:55:34 -0400

    distance modded

--- rectturtle.py
+++ rectturtle.py
@@ -16,6 +16,20 @@ def draw_rectangle(width, height):
   bob.lt(90)
 
 
+bob1=turtle.Turtle()
+def polygon_circle(bob1,n,length):
+
+  for i in range(n):
+    bob1.left(360/n)
+    bob1.forward(length)
+def draw_circle(bob1,r):
+  circumfurence=2*(22/7)*r
+  n=50
+  length=circumfurence/n
+  polygon_circle(bob1,n,length)
+
+#bob1.exitonclick()
+print(draw_circle(bob1,100))
 
 print(draw_rectangle(width,height))
 turtle.mainloop()
